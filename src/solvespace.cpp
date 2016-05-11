e129755d660fd51d484906e218cd629844b680c6
whitequark <whitequark@whitequark.org>
2016-05-11 07:43:04 +0000

    Offer to save file when closing if it is unsaved.

--- src/solvespace.cpp
+++ src/solvespace.cpp
@@ -144,6 +144,9 @@ bool SolveSpaceUI::OpenFile(const std::string &filename) {
 }
 
 void SolveSpaceUI::Exit(void) {
+    if(!OkayToStartNewFile())
+        return;
+
     // Recent files
     for(int i = 0; i < MAX_RECENT; i++)
         CnfFreezeString(RecentFile[i], "RecentFile_" + std::to_string(i));
