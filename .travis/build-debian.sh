415a6d36ff05bfcd563b8dc741969f1c5c5beaf9
whitequark <whitequark@whitequark.org>
2016-08-07 17:11:51 +0000

    Travis: build with clang, so that we can use its sanitizers.
    This also disables coverage, because llvm-cov 3.9 on Travis is
    incompatible with genhtml 1.10. Sigh.

--- .travis/build-debian.sh
+++ .travis/build-debian.sh
@@ -4,8 +4,7 @@ if echo $TRAVIS_TAG | grep ^v; then BUILD_TYPE=RelWithDebInfo; else BUILD_TYPE=D
 
 mkdir build
 cd build
-cmake .. -DCMAKE_C_COMPILER=gcc-5 -DCMAKE_CXX_COMPILER=g++-5 \
+cmake .. -DCMAKE_C_COMPILER=clang-3.9 -DCMAKE_CXX_COMPILER=clang++-3.9 \
   -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-  -DENABLE_COVERAGE=ON \
   -DENABLE_SANITIZERS=ON
 make VERBOSE=1
