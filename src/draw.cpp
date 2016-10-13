solvespace/solvespace

commit df83ee4c8ffdacf13dea4b195820bbf5b40db5f2

Factor out Style::Stroke.

src/draw.cpp
@@ -650,10 +650,8 @@ void GraphicsWindow::Draw(Canvas *canvas) {
         c.Draw(Constraint::DrawAs::DEFAULT, canvas);
     }
 
-    Canvas::Stroke strokeAnalyze = {};
+    Canvas::Stroke strokeAnalyze = Style::Stroke(Style::ANALYZE);
     strokeAnalyze.layer = Canvas::Layer::FRONT;
-    strokeAnalyze.color = Style::Color(Style::ANALYZE);
-    strokeAnalyze.width = Style::Width(Style::ANALYZE);
     Canvas::hStroke hcsAnalyze = canvas->GetStroke(strokeAnalyze);
 
     // Draw the traced path, if one exists
@@ -662,9 +660,8 @@ void GraphicsWindow::Draw(Canvas *canvas) {
     canvas->DrawEdges(tracedEdges, hcsAnalyze);
     tracedEdges.Clear();
 
-    Canvas::Stroke strokeError = {};
+    Canvas::Stroke strokeError = Style::Stroke(Style::DRAW_ERROR);
     strokeError.layer = Canvas::Layer::FRONT;
-    strokeError.color = Style::Color(Style::DRAW_ERROR);
     strokeError.width = 12;
     Canvas::hStroke hcsError = canvas->GetStroke(strokeError);
 
@@ -681,9 +678,8 @@ void GraphicsWindow::Draw(Canvas *canvas) {
     }
     SK.GetGroup(activeGroup)->DrawMesh(Group::DrawMeshAs::SELECTED, canvas);
 
-    Canvas::Stroke strokeDatum = {};
+    Canvas::Stroke strokeDatum = Style::Stroke(Style::DATUM);
     strokeDatum.layer = Canvas::Layer::FRONT;
-    strokeDatum.color = Style::Color(Style::DATUM);
     strokeDatum.width = 1;
     Canvas::hStroke hcsDatum = canvas->GetStroke(strokeDatum);
 

src/drawentity.cpp
@@ -464,13 +464,9 @@ void Entity::Draw(DrawAs how, Canvas *canvas) {
     }
 
     Canvas::Stroke stroke = {};
-    stroke.zIndex         = zIndex;
-    stroke.color          = Style::Color(hs);
-    stroke.width          = Style::Width(hs);
-    stroke.stipplePattern = Style::PatternType(hs);
-    stroke.stippleScale   = Style::StippleScaleMm(hs);
     switch(how) {
         case DrawAs::DEFAULT:
+            stroke = Style::Stroke(hs);
             stroke.layer  = Canvas::Layer::NORMAL;
             break;
 
@@ -479,21 +475,23 @@ void Entity::Draw(DrawAs how, Canvas *canvas) {
             break;
 
         case DrawAs::HIDDEN:
+            stroke = Style::Stroke(Style::HIDDEN_EDGE);
             stroke.layer  = Canvas::Layer::OCCLUDED;
-            stroke.stipplePattern = Style::PatternType({ Style::HIDDEN_EDGE });
-            stroke.stippleScale   = Style::StippleScaleMm({ Style::HIDDEN_EDGE });
             break;
 
         case DrawAs::HOVERED:
+            stroke = Style::Stroke(hs);
             stroke.layer  = Canvas::Layer::FRONT;
             stroke.color  = Style::Color(Style::HOVERED);
             break;
 
         case DrawAs::SELECTED:
+            stroke = Style::Stroke(hs);
             stroke.layer  = Canvas::Layer::FRONT;
             stroke.color  = Style::Color(Style::SELECTED);
             break;
     }
+    stroke.zIndex = zIndex;
     Canvas::hStroke hcs = canvas->GetStroke(stroke);
 
     Canvas::Stroke pointStroke = {};
@@ -529,8 +527,7 @@ void Entity::Draw(DrawAs how, Canvas *canvas) {
                 free = pu->free || pv->free;
             }
             if(free) {
-                Canvas::Stroke analyzeStroke = pointStroke;
-                analyzeStroke.color = Style::Color(Style::ANALYZE);
+                Canvas::Stroke analyzeStroke = Style::Stroke(Style::ANALYZE);
                 analyzeStroke.width = 14.0;
                 Canvas::hStroke hcsAnalyze = canvas->GetStroke(analyzeStroke);
 

src/groupmesh.cpp
@@ -529,10 +529,8 @@ void Group::Draw(Canvas *canvas) {
     DrawMesh(DrawMeshAs::DEFAULT, canvas);
 
     if(SS.GW.showEdges) {
-        Canvas::Stroke strokeEdge = {};
+        Canvas::Stroke strokeEdge = Style::Stroke(Style::SOLID_EDGE);
         strokeEdge.zIndex = 1;
-        strokeEdge.color  = Style::Color(Style::SOLID_EDGE);
-        strokeEdge.width  = Style::Width(Style::SOLID_EDGE);
         Canvas::hStroke hcsEdge = canvas->GetStroke(strokeEdge);
 
         canvas->DrawOutlines(displayOutlines, hcsEdge,
@@ -541,11 +539,8 @@ void Group::Draw(Canvas *canvas) {
                              : Canvas::DrawOutlinesAs::EMPHASIZED_AND_CONTOUR);
 
         if(SS.GW.drawOccludedAs == GraphicsWindow::DrawOccludedAs::STIPPLED) {
-            Canvas::Stroke strokeHidden = strokeEdge;
+            Canvas::Stroke strokeHidden = Style::Stroke(Style::HIDDEN_EDGE);
             strokeHidden.layer  = Canvas::Layer::OCCLUDED;
-            strokeHidden.width = Style::Width(Style::HIDDEN_EDGE);
-            strokeHidden.stipplePattern = Style::PatternType({ Style::HIDDEN_EDGE });
-            strokeHidden.stippleScale   = Style::StippleScaleMm({ Style::HIDDEN_EDGE });
             Canvas::hStroke hcsHidden = canvas->GetStroke(strokeHidden);
 
             canvas->DrawOutlines(displayOutlines, hcsHidden,
@@ -568,14 +563,13 @@ void Group::Draw(Canvas *canvas) {
 void Group::DrawPolyError(Canvas *canvas) {
     const Camera &camera = canvas->GetCamera();
 
-    Canvas::Stroke strokeUnclosed = {};
-    strokeUnclosed.color = Style::Color(Style::DRAW_ERROR).WithAlpha(50);
-    strokeUnclosed.width = Style::Width(Style::DRAW_ERROR);
+    Canvas::Stroke strokeUnclosed = Style::Stroke(Style::DRAW_ERROR);
+    strokeUnclosed.color = strokeUnclosed.color.WithAlpha(50);
     Canvas::hStroke hcsUnclosed = canvas->GetStroke(strokeUnclosed);
 
-    Canvas::Stroke strokeError = {};
-    strokeError.layer    = Canvas::Layer::FRONT;
-    strokeError.color    = Style::Color(Style::DRAW_ERROR);
+    Canvas::Stroke strokeError = Style::Stroke(Style::DRAW_ERROR);
+    strokeError.layer = Canvas::Layer::FRONT;
+    strokeError.width = 0.0f;
     Canvas::hStroke hcsError = canvas->GetStroke(strokeError);
 
     double textHeight = Style::DefaultTextHeight() / camera.scale;

src/style.cpp
@@ -282,6 +282,23 @@ double Style::DefaultTextHeight() {
 }
 
 //-----------------------------------------------------------------------------
+// Return the parameters of this style, as a canvas stroke.
+//-----------------------------------------------------------------------------
+Canvas::Stroke Style::Stroke(hStyle hs) {
+    Canvas::Stroke stroke = {};
+    Style *style = Style::Get(hs);
+    stroke.color = style->color;
+    stroke.stipplePattern = style->stippleType;
+    stroke.stippleScale = Style::StippleScaleMm(hs);
+    stroke.width = Style::Width(hs.v);
+    return stroke;
+}
+
+Canvas::Stroke Style::Stroke(int hsv) {
+    return Style::Stroke(hStyle{hsv});
+}
+
+//-----------------------------------------------------------------------------
 // Should lines and curves from this style appear in the output file? Only
 // if it's both shown and exportable.
 //-----------------------------------------------------------------------------
@@ -892,4 +909,3 @@ void TextWindow::ShowStyleInfo() {
 void TextWindow::ScreenAssignSelectionToStyle(int link, uint32_t v) {
     Style::AssignSelectionToStyle(v);
 }
-

src/drawconstraint.cpp
@@ -509,11 +509,9 @@ void Constraint::DoLayout(DrawAs how, Canvas *canvas,
         case DrawAs::HOVERED:  color = Style::Color(Style::HOVERED);    break;
         case DrawAs::SELECTED: color = Style::Color(Style::SELECTED);   break;
     }
-
-    Canvas::Stroke stroke = {};
+    Canvas::Stroke stroke = Style::Stroke(GetStyle());
     stroke.layer    = Canvas::Layer::FRONT;
     stroke.color    = color;
-    stroke.width    = Style::Width(GetStyle());
     stroke.zIndex   = 4;
     Canvas::hStroke hcs = canvas->GetStroke(stroke);
 

src/sketch.h
@@ -813,13 +813,15 @@ public:
 
     static Style *Get(hStyle hs);
     static RgbaColor Color(hStyle hs, bool forExport=false);
+    static RgbaColor Color(int hs, bool forExport=false);
     static RgbaColor FillColor(hStyle hs, bool forExport=false);
     static double Width(hStyle hs);
-    static RgbaColor Color(int hs, bool forExport=false);
     static double Width(int hs);
     static double WidthMm(int hs);
     static double TextHeight(hStyle hs);
     static double DefaultTextHeight();
+    static Canvas::Stroke Stroke(hStyle hs);
+    static Canvas::Stroke Stroke(int hs);
     static bool Exportable(int hs);
     static hStyle ForEntity(hEntity he);
     static StipplePattern PatternType(hStyle hs);

commit d2c250324b2f9833ef9f4231c5145c4fd11abab7

Fix many rendering bugs introduced in df83ee4 and 9f97e9a.

src/draw.cpp
@@ -60,6 +60,7 @@ void GraphicsWindow::Selection::Draw(bool isHovered, Canvas *canvas) {
         strokeEmphasis.layer  = Canvas::Layer::FRONT;
         strokeEmphasis.color  = Style::Color(Style::HOVERED).WithAlpha(50);
         strokeEmphasis.width  = 40;
+        strokeEmphasis.unit   = Canvas::Unit::PX;
         Canvas::hStroke hcsEmphasis = canvas->GetStroke(strokeEmphasis);
 
         Point2d topLeftScreen;
@@ -545,6 +546,8 @@ void GraphicsWindow::DrawSnapGrid(Canvas *canvas) {
     Canvas::Stroke stroke = {};
     stroke.layer  = Canvas::Layer::BACK;
     stroke.color  = Style::Color(Style::DATUM).WithAlpha(75);
+    stroke.unit   = Canvas::Unit::PX;
+    stroke.width  = 1.0f;
     Canvas::hStroke hcs = canvas->GetStroke(stroke);
 
     for(i = i0 + 1; i < i1; i++) {
@@ -679,6 +682,7 @@ void GraphicsWindow::Draw(Canvas *canvas) {
     SK.GetGroup(activeGroup)->DrawMesh(Group::DrawMeshAs::SELECTED, canvas);
 
     Canvas::Stroke strokeDatum = Style::Stroke(Style::DATUM);
+    strokeDatum.unit  = Canvas::Unit::PX;
     strokeDatum.layer = Canvas::Layer::FRONT;
     strokeDatum.width = 1;
     Canvas::hStroke hcsDatum = canvas->GetStroke(strokeDatum);

src/drawentity.cpp
@@ -463,32 +463,30 @@ void Entity::Draw(DrawAs how, Canvas *canvas) {
         hs = Style::ForEntity(h);
     }
 
-    Canvas::Stroke stroke = {};
+    Canvas::Stroke stroke = Style::Stroke(hs);
     switch(how) {
         case DrawAs::DEFAULT:
-            stroke = Style::Stroke(hs);
-            stroke.layer  = Canvas::Layer::NORMAL;
+            stroke.layer = Canvas::Layer::NORMAL;
             break;
 
         case DrawAs::OVERLAY:
-            stroke.layer  = Canvas::Layer::FRONT;
+            stroke.layer = Canvas::Layer::FRONT;
             break;
 
         case DrawAs::HIDDEN:
-            stroke = Style::Stroke(Style::HIDDEN_EDGE);
-            stroke.layer  = Canvas::Layer::OCCLUDED;
+            stroke.layer = Canvas::Layer::OCCLUDED;
+            stroke.stipplePattern = Style::PatternType({ Style::HIDDEN_EDGE });
+            stroke.stippleScale   = Style::Get({ Style::HIDDEN_EDGE })->stippleScale;
             break;
 
         case DrawAs::HOVERED:
-            stroke = Style::Stroke(hs);
-            stroke.layer  = Canvas::Layer::FRONT;
-            stroke.color  = Style::Color(Style::HOVERED);
+            stroke.layer = Canvas::Layer::FRONT;
+            stroke.color = Style::Color(Style::HOVERED);
             break;
 
         case DrawAs::SELECTED:
-            stroke = Style::Stroke(hs);
-            stroke.layer  = Canvas::Layer::FRONT;
-            stroke.color  = Style::Color(Style::SELECTED);
+            stroke.layer = Canvas::Layer::FRONT;
+            stroke.color = Style::Color(Style::SELECTED);
             break;
     }
     stroke.zIndex = zIndex;

src/groupmesh.cpp
@@ -479,6 +479,7 @@ void Group::DrawMesh(DrawMeshAs how, Canvas *canvas) {
                 strokeTriangle.zIndex = 1;
                 strokeTriangle.color  = RgbaColor::FromFloat(0.0f, 1.0f, 0.0f);
                 strokeTriangle.width  = 1;
+                strokeTriangle.unit = Canvas::Unit::PX;
                 hcsTriangle = canvas->GetStroke(strokeTriangle);
             }
 
@@ -551,10 +552,8 @@ void Group::Draw(Canvas *canvas) {
     }
 
     if(SS.GW.showOutlines) {
-        Canvas::Stroke strokeOutline = {};
+        Canvas::Stroke strokeOutline = Style::Stroke(Style::OUTLINE);
         strokeOutline.zIndex = 1;
-        strokeOutline.color  = Style::Color(Style::OUTLINE);
-        strokeOutline.width  = Style::Width(Style::OUTLINE);
         Canvas::hStroke hcsOutline = canvas->GetStroke(strokeOutline);
 
         canvas->DrawOutlines(displayOutlines, hcsOutline,

src/render/render.cpp
@@ -133,6 +133,7 @@ bool Canvas::Stroke::Equals(const Stroke &other) const {
             zIndex == other.zIndex &&
             color.Equals(other.color) &&
             width == other.width &&
+            unit == other.unit &&
             stipplePattern == other.stipplePattern &&
             stippleScale == other.stippleScale);
 }
@@ -221,6 +222,7 @@ void UiCanvas::DrawLine(int x1, int y1, int x2, int y2, RgbaColor color, int wid
     stroke.layer  = Canvas::Layer::FRONT;
     stroke.width  = (double)width;
     stroke.color  = color;
+    stroke.unit   = Canvas::Unit::PX;
     Canvas::hStroke hcs = canvas->GetStroke(stroke);
 
     canvas->DrawLine(va, vb, hcs);
@@ -247,6 +249,7 @@ void UiCanvas::DrawRect(int l, int r, int t, int b,
         stroke.layer  = Canvas::Layer::FRONT;
         stroke.width  = 1.0;
         stroke.color  = outlineColor;
+        stroke.unit   = Canvas::Unit::PX;
         Canvas::hStroke hcs = canvas->GetStroke(stroke);
 
         canvas->DrawLine(va, vb, hcs);

src/style.cpp
@@ -289,7 +289,7 @@ Canvas::Stroke Style::Stroke(hStyle hs) {
     Style *style = Style::Get(hs);
     stroke.color = style->color;
     stroke.stipplePattern = style->stippleType;
-    stroke.stippleScale = Style::StippleScaleMm(hs);
+    stroke.stippleScale = style->stippleScale;
     stroke.width = Style::Width(hs.v);
     switch(style->widthAs) {
         case Style::UnitsAs::PIXELS: