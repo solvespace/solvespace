fc31c31fb2c053168f4d76113d8ce6cab1a564a0
Dynamo Dan <info@dynamodan.com>
2017-01-13 22:42:13 -0500

    Issue #123: fix a bug affecting horizon tilt when facing toward Y

--- src/mouse.cpp
+++ src/mouse.cpp
@@ -129,10 +129,10 @@ void GraphicsWindow::MouseMoved(double x, double y, bool leftDown,
 
         if(!(shiftDown || ctrlDown)) {
             double s = 0.3*(PI/180)*scale; // degrees per pixel
-            if(SS.turntableNav) {
+            if(SS.turntableNav) { // lock the Z to vertical
                 projRight = orig.projRight.RotatedAbout(Vector::From(0, 0, 1), -s*dx);
-                projUp = orig.projUp.RotatedAbout( // lock the Z to vertical
-                    Vector::From(orig.projRight.x, 0, orig.projRight.y),
+                projUp = orig.projUp.RotatedAbout(
+                    Vector::From(orig.projRight.x, orig.projRight.y, orig.projRight.y),
                     s*dy
                 );
             } else {
