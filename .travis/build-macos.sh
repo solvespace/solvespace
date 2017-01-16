04e86d9b368bd32f36535cbea53a80a31b0c2c61
whitequark <whitequark@whitequark.org>
2017-01-16 12:29:29 +0000

    Travis: revert the hack for transient build failures.

--- .travis/build-macos.sh
+++ .travis/build-macos.sh
@@ -5,16 +5,4 @@ if echo $TRAVIS_TAG | grep ^v; then BUILD_TYPE=RelWithDebInfo; else BUILD_TYPE=D
 mkdir build
 cd build
 cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7 -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
-if ! make VERBOSE=1; then
-  echo "Sigh, transient build failure. Retrying..."
-  if ! make VERBOSE=1; then
-    echo "Another transient build failure. Retrying..."
-    if ! make VERBOSE=1; then
-      echo "I can't believe how deep the Travis brokenness goes. Retrying..."
-      if ! make VERBOSE=1; then
-        echo "Okay, this is probably an actual bug."
-        exit 1
-      fi
-    fi
-  fi
-fi
+make VERBOSE=1
