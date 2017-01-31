solvespace/solvespace

commit 3fc85b79341fb3d5de44e8a9b3924f95f0a07cac

CLI: add the "regenerate" command, for use in Makefiles etc.

src/platform/climain.cpp
@@ -46,6 +46,8 @@ static void ShowUsage(const std::string &cmd) {
         being triangulated first.
     export-surfaces --output <pattern>
         Exports exact surfaces of solids in the sketch, if any.
+    regenerate
+        Reloads all imported files, regenerates the sketch, and saves it.
 )");
 
     auto FormatListFromFileFilter = [](const FileFilter *filter) {
@@ -265,6 +267,20 @@ static bool RunCommand(const std::vector<std::string> args) {
         runner = [&](const std::string &output) {
             StepFileWriter sfw = {};
             sfw.ExportSurfacesTo(output);
+        };
+    } else if(args[1] == "regenerate") {
+        for(size_t argn = 2; argn < args.size(); argn++) {
+            if(!(ParseInputFile(argn))) {
+                fprintf(stderr, "Unrecognized option '%s'.\n", args[argn].c_str());
+                return false;
+            }
+        }
+
+        outputPattern = "%.slvs";
+
+        runner = [&](const std::string &output) {
+            SS.ReloadAllImported();
+            SS.SaveToFile(output);
         };
     } else {
         fprintf(stderr, "Unrecognized command '%s'.\n", args[1].c_str());

commit 6e88eaef69b718048cd8a8b1d1cd8b7dbc8704cc

Fix a path construction issue introduced in 3fc85b7.

src/platform/climain.cpp
@@ -309,7 +309,9 @@ static bool RunCommand(const std::vector<std::string> args) {
         if(replaceAt != std::string::npos) {
             std::string outputSubst;
             outputSubst  = Dirname(inputFile);
-            outputSubst += PATH_SEP;
+            if(!outputSubst.empty()) {
+                outputSubst += PATH_SEP;
+            }
             outputSubst += Basename(inputFile, /*stripExtension=*/true);
             outputFile.replace(replaceAt, 1, outputSubst);
         }