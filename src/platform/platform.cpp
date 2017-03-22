6337bbbcd6e753427bed47d7e9f43d9836e4347b
whitequark <whitequark@whitequark.org>
2017-03-22 19:13:00 +0000

    Fix a missing semicolon.

--- src/platform/platform.cpp
+++ src/platform/platform.cpp
@@ -504,7 +504,7 @@ static Platform::Path ResourcePath(const std::string &name) {
 #    if defined(__linux__)
 static const char *selfSymlink = "/proc/self/exe";
 #    elif defined(__NetBSD__)
-static const char *selfSymlink = "/proc/curproc/exe"
+static const char *selfSymlink = "/proc/curproc/exe";
 #    elif defined(__OpenBSD__) || defined(__FreeBSD__)
 static const char *selfSymlink = "/proc/curproc/file";
 #    else
