f961a71b323124a876cf321dc4e83bfbcb08dbd9
phkahler <14852918+phkahler@users.noreply.github.com>
2019-09-08 22:03:31 -0400

    Fix #472 don't assume u,v derivatives are orthogonal in ClosestPointNewton

--- src/srf/ratpoly.cpp
+++ src/srf/ratpoly.cpp
@@ -501,21 +501,27 @@ bool SSurface::ClosestPointNewton(Vector p, double *u, double *v, bool mustConve
             }
         }
 
-        Vector tu, tv;
+        Vector tu, tv, tx, ty;
         TangentsAt(*u, *v, &tu, &tv);
+        // since tu and tv may not be orthogonal, use y in place of v.
+        // |y| = |v|sin(theta) where theta is the angle between tu and tv.
+        ty = tu.Cross(tv).Cross(tu).ScaledBy(1.0/tu.MagSquared());
+        tx = tv.Cross(tu).Cross(tv).ScaledBy(1.0/tv.MagSquared());
 
         // Project the point into a plane through p0, with basis tu, tv; a
         // second-order thing would converge faster but needs second
         // derivatives.
         Vector dp = p.Minus(p0);
-        double du = dp.Dot(tu), dv = dp.Dot(tv);
-        *u += du / (tu.MagSquared());
-        *v += dv / (tv.MagSquared());
+        double du = dp.Dot(tx),
+               dv = dp.Dot(ty);
+        *u += du / (tu.Magnitude() * tx.Magnitude());
+        *v += dv / (tv.Magnitude() * ty.Magnitude());
 
         if (*u < 0.0) *u = 0.0;
         else if (*u > 1.0) *u = 1.0;
         if (*v < 0.0) *v = 0.0;
         else if (*v > 1.0) *v = 1.0;
+
     }
 
     if(mustConverge) {
