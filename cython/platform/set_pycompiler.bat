bd616e898d61fb9aa70eef1d2e239d08655df5a4
KmolYuan <pyslvs@gmail.com>
2019-10-08 21:25:03 +0800

    Add MSVC CI builder.

--- cython/platform/set_pycompiler.bat
+++ cython/platform/set_pycompiler.bat
@@ -1,14 +1,16 @@
 echo off
 
-REM Usage: set_pycompiler C:\Python37
+REM Usage: set_pycompiler C:\Python37 mingw32
 REM Where %PYTHON_DIR% is the directory of your Python installation.
+REM Compiler option can be "mingw32" or "msvc".
 REM In Pyslvs project.
 set HERE=%~dp0
 set PYTHON_DIR=%1
+set COMPILER=%2
 
 REM Create "distutils.cfg"
 echo [build]>> "%PYTHON_DIR%\Lib\distutils\distutils.cfg"
-echo compiler=mingw32>> "%PYTHON_DIR%\Lib\distutils\distutils.cfg"
+echo compiler=%COMPILER%>> "%PYTHON_DIR%\Lib\distutils\distutils.cfg"
 
 REM Apply the patch of "cygwinccompiler.py".
 REM Unix "patch" command of Msys.
