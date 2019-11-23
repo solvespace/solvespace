9c2218a5a8d81b2b1b0e7cde794c3758b481fc27
Maximilian Federle <max.federle@googlemail.com>
2019-11-23 21:45:32 +0100

    snap: Build snaps on Travis via remote-build
    We invoke builds on Launchpad in stage "deploy"
    and release it into the edge channel of the Snap Store.
    
    The deploy stage is blocked on fails of the test stage,
    so we don't release snaps with failing tests.

---
+++ .travis/build-snap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -xe
+
+lp_data_dir="$HOME/.local/share/snapcraft/provider/launchpad"
+lp_credentials="$lp_data_dir/credentials"
+
+mkdir -p "$lp_data_dir"
+openssl aes-256-cbc -K $encrypted_02880a344e9c_key -iv $encrypted_02880a344e9c_iv \
+  -in .travis/launchpad-credentials.enc \
+  -out "$lp_credentials" -d
+chmod 600 "$lp_credentials" 
+
+./pkg/snap/build.sh remote-build --launchpad-user ppd --launchpad-accept-public-upload --build-on=amd64
