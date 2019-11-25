a60d4df179e24561b26556b7fdcc5e8af23dc41b
Maximilian Federle <max.federle@googlemail.com>
2019-11-25 18:28:42 +0100

    Refactor Travis config
    - Fix invalid osx_image xcode8.2
    - Drop depreceated sudo keyword
    - Run debian build on bionic image & re-enable building with ui
    - Split build into separate jobs & stages for better separation of concerns

--- .travis/build-debian.sh
+++ .travis/build-debian.sh
@@ -4,12 +4,8 @@ if echo $TRAVIS_TAG | grep ^v; then BUILD_TYPE=RelWithDebInfo; else BUILD_TYPE=D
 
 mkdir build
 cd build
-# We build without the GUI until Travis updates to an Ubuntu version with GTK 3.16+.
 cmake .. \
-  -DCMAKE_C_COMPILER=gcc-5 \
-  -DCMAKE_CXX_COMPILER=g++-5 \
   -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-  -DENABLE_GUI=OFF \
   -DENABLE_SANITIZERS=ON
 make VERBOSE=1
 make test_solvespace
--- .travis/install-debian.sh
+++ .travis/install-debian.sh
@@ -1,8 +1,10 @@
 #!/bin/sh -xe
 
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
+
 sudo apt-get install -q -y \
-  cmake cmake-data libc6-dev libpng12-dev zlib1g-dev libjson0-dev libfontconfig1-dev \
-  libgtkmm-3.0-dev libpangomm-1.4-dev libcairo2-dev libgl1-mesa-dev libglu-dev \
-  libfreetype6-dev dpkg-dev gcc-5 g++-5 lcov
+  zlib1g-dev libpng-dev libcairo2-dev libfreetype6-dev libjson-c-dev \
+  libfontconfig1-dev libgtkmm-3.0-dev libpangomm-1.4-dev libgl-dev \
+  libgl-dev libglu-dev libspnav-dev 
+
+git submodule update --init extlib/libdxfrw extlib/flatbuffers extlib/q3d
