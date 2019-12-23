3ef465984689ffb28edfcabfd69800d892d37933
ruevs <dpr@ruevs.com>
2019-12-23 23:56:34 +0200

    Win32: Open a console in debug mode to display stderr debug messages.

--- src/platform/guiwin.cpp
+++ src/platform/guiwin.cpp
@@ -1665,6 +1665,16 @@ void InitGui(int argc, char **argv) {
     if(!SetLocale((uint16_t)GetUserDefaultLCID())) {
         SetLocale("en_US");
     }
+
+#ifndef NDEBUG
+    // create a debug console
+    if(AllocConsole()) {
+        (void)freopen("CONOUT$", "w", /*stdout*/stderr);
+        SetConsoleTitle(L"Debug Console");
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),
+                                FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED);
+    }
+#endif
 }
 
 void RunGui() {
