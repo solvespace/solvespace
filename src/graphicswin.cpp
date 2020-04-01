7ffea4cb2bbec900783cffe85fcdeb7880e159c4
Tim Hutt <tdhutt@gmail.com>
2020-04-01 21:28:15 +0100

    Add ifndef __APPLE__

--- src/graphicswin.cpp
+++ src/graphicswin.cpp
@@ -77,7 +77,9 @@ const MenuEntry Menu[] = {
 { 1,  NULL,                             Command::NONE,             0,       KN, NULL   },
 { 1, N_("&Line Styles..."),             Command::EDIT_LINE_STYLES, 0,       KN, mEdit  },
 { 1, N_("&View Projection..."),         Command::VIEW_PROJECTION,  0,       KN, mEdit  },
+#ifndef __APPLE__
 { 1, N_("Con&figuration..."),           Command::CONFIGURATION,    0,       KN, mEdit  },
+#endif
 
 { 0, N_("&View"),                       Command::NONE,             0,       KN, mView  },
 { 1, N_("Zoom &In"),                    Command::ZOOM_IN,          '+',     KN, mView  },
