46979db60d305aae2a945270e1a715eb024573ac
KmolYuan <pyslvs@gmail.com>
2020-10-05 14:29:02 +0800

    Try to fix the CLang problem.

--- cython/setup.py
+++ cython/setup.py
@@ -9,7 +9,7 @@ __email__ = "pyslvs@gmail.com"
 
 import sys
 from os import walk
-from os.path import dirname, isdir, join as pth_join
+from os.path import dirname, isdir, join, abspath
 import re
 import codecs
 from setuptools import setup, Extension, find_packages
@@ -19,22 +19,23 @@ from distutils import file_util, dir_util
 from platform import system
 
 m_path = 'python_solvespace'
-include_path = pth_join(m_path, 'include')
-src_path = pth_join(m_path, 'src')
-platform_path = pth_join(src_path, 'platform')
-extlib_path = pth_join(m_path, 'extlib')
-mimalloc_path = pth_join(extlib_path, 'mimalloc')
-mimalloc_include_path = pth_join(mimalloc_path, 'include')
-mimalloc_src_path = pth_join(mimalloc_path, 'src')
+include_path = join(m_path, 'include')
+src_path = join(m_path, 'src')
+platform_path = join(src_path, 'platform')
+extlib_path = join(m_path, 'extlib')
+mimalloc_path = join(extlib_path, 'mimalloc')
+mimalloc_include_path = join(mimalloc_path, 'include')
+mimalloc_src_path = join(mimalloc_path, 'src')
+build_dir = join(abspath(dirname(__file__)), 'build')
 
 
 def write(doc, *parts):
-    with codecs.open(pth_join(*parts), 'w') as f:
+    with codecs.open(join(*parts), 'w') as f:
         f.write(doc)
 
 
 def read(*parts):
-    with codecs.open(pth_join(*parts), 'r') as f:
+    with codecs.open(join(*parts), 'r') as f:
         return f.read()
 
 
@@ -60,9 +61,12 @@ compile_args = [
     '-Wno-write-strings',
     '-fpermissive',
     '-fPIC',
+    '-std=c++17',
 ]
-if system() != 'Darwin':
-    compile_args.append('-std=c++17')
+compile_args_msvc = [
+    '/O2',
+    '/std:c++17',
+]
 link_args = ['-static-libgcc', '-static-libstdc++',
              '-Wl,-Bstatic,--whole-archive',
              '-lwinpthread',
@@ -71,32 +75,34 @@ link_args = ['-static-libgcc', '-static-libstdc++',
              '-lpsapi',
              '-Wl,-Bdynamic']
 sources = [
-    pth_join(m_path, 'slvs.pyx'),
-    pth_join(src_path, 'util.cpp'),
-    pth_join(src_path, 'entity.cpp'),
-    pth_join(src_path, 'expr.cpp'),
-    pth_join(src_path, 'constraint.cpp'),
-    pth_join(src_path, 'constrainteq.cpp'),
-    pth_join(src_path, 'system.cpp'),
-    pth_join(src_path, 'lib.cpp'),
-    pth_join(platform_path, 'platform.cpp'),
+    join(m_path, 'slvs.pyx'),
+    join(src_path, 'util.cpp'),
+    join(src_path, 'entity.cpp'),
+    join(src_path, 'expr.cpp'),
+    join(src_path, 'constraint.cpp'),
+    join(src_path, 'constrainteq.cpp'),
+    join(src_path, 'system.cpp'),
+    join(src_path, 'lib.cpp'),
+    join(platform_path, 'platform.cpp'),
+]
+mimalloc_sources = [
     # MiMalloc
-    pth_join(mimalloc_src_path, 'stats.c'),
-    pth_join(mimalloc_src_path, 'random.c'),
-    pth_join(mimalloc_src_path, 'os.c'),
-    pth_join(mimalloc_src_path, 'arena.c'),
-    pth_join(mimalloc_src_path, 'region.c'),
-    pth_join(mimalloc_src_path, 'segment.c'),
-    pth_join(mimalloc_src_path, 'page.c'),
-    pth_join(mimalloc_src_path, 'alloc.c'),
-    pth_join(mimalloc_src_path, 'alloc-aligned.c'),
-    pth_join(mimalloc_src_path, 'alloc-posix.c'),
-    pth_join(mimalloc_src_path, 'heap.c'),
-    pth_join(mimalloc_src_path, 'options.c'),
-    pth_join(mimalloc_src_path, 'init.c'),
+    join(mimalloc_src_path, 'stats.c'),
+    join(mimalloc_src_path, 'random.c'),
+    join(mimalloc_src_path, 'os.c'),
+    join(mimalloc_src_path, 'arena.c'),
+    join(mimalloc_src_path, 'region.c'),
+    join(mimalloc_src_path, 'segment.c'),
+    join(mimalloc_src_path, 'page.c'),
+    join(mimalloc_src_path, 'alloc.c'),
+    join(mimalloc_src_path, 'alloc-aligned.c'),
+    join(mimalloc_src_path, 'alloc-posix.c'),
+    join(mimalloc_src_path, 'heap.c'),
+    join(mimalloc_src_path, 'options.c'),
+    join(mimalloc_src_path, 'init.c'),
 ]
 if {'sdist', 'bdist'} & set(sys.argv):
-    sources.append(pth_join(platform_path, 'platform.cpp'))
+    sources.append(join(platform_path, 'platform.cpp'))
 elif system() == 'Windows':
     # Disable format warning
     compile_args.append('-Wno-format')
@@ -110,24 +116,26 @@ compiler_directives = {'binding': True, 'cdivision': True}
 
 
 def copy_source(dry_run):
-    dir_util.copy_tree(pth_join('..', 'include'), include_path, dry_run=dry_run)
-    dir_util.copy_tree(pth_join('..', 'extlib', 'mimalloc', 'include'), include_path, dry_run=dry_run)
+    dir_util.copy_tree(join('..', 'include'), include_path, dry_run=dry_run)
+    dir_util.copy_tree(join('..', 'extlib', 'mimalloc', 'include'),
+                       mimalloc_include_path,
+                       dry_run=dry_run)
     dir_util.mkpath(src_path)
     dir_util.mkpath(mimalloc_src_path)
-    for path in (pth_join('..', 'src'), pth_join('..', 'extlib', 'mimalloc', 'src')):
+    for path in (join('..', 'src'), join('..', 'extlib', 'mimalloc', 'src')):
         for root, _, files in walk(path):
             for f in files:
                 if not (f.endswith('.h') or f.endswith('.c')):
                     continue
-                f = pth_join(root, f)
+                f = join(root, f)
                 f_new = f.replace('..', m_path)
                 if not isdir(dirname(f_new)):
                     dir_util.mkpath(dirname(f_new))
                 file_util.copy_file(f, f_new, dry_run=dry_run)
-    for f in sources[1:]:
+    for f in sources[1:] + mimalloc_sources:
         file_util.copy_file(f.replace(m_path, '..'), f, dry_run=dry_run)
     # Create an empty header
-    open(pth_join(platform_path, 'config.h'), 'a').close()
+    open(join(platform_path, 'config.h'), 'a').close()
 
 
 class Build(build_ext):
@@ -135,6 +143,8 @@ class Build(build_ext):
         compiler = self.compiler.compiler_type
         for e in self.extensions:
             e.cython_directives = compiler_directives
+            e.libraries = ['mimalloc']
+            e.library_dirs = [build_dir]
             if compiler in {'mingw32', 'unix'}:
                 e.define_macros = macros
                 e.extra_compile_args = compile_args
@@ -142,15 +152,24 @@ class Build(build_ext):
                     e.extra_link_args = link_args
             elif compiler == 'msvc':
                 e.define_macros = macros[1:]
-                e.libraries = ['shell32', 'advapi32', 'Ws2_32']
-                e.extra_compile_args = ['/O2', '/std:c++17']
-        super(Build, self).build_extensions()
-
-    def run(self):
+                e.libraries.extend(['shell32', 'advapi32', 'Ws2_32'])
+                e.extra_compile_args = compile_args_msvc
         has_src = isdir(include_path) and isdir(src_path) and isdir(extlib_path)
         if not has_src:
             copy_source(self.dry_run)
-        super(Build, self).run()
+        # Pre-build MiMalloc
+        if compiler in {'mingw32', 'unix'}:
+            args = ['-fPIC']
+        else:
+            args = []
+        objects = self.compiler.compile(
+            mimalloc_sources,
+            extra_postargs=args,
+            include_dirs=[mimalloc_include_path, mimalloc_src_path]
+        )
+        self.compiler.create_static_lib(objects, 'mimalloc', target_lang='c',
+                                        output_dir=build_dir)
+        super(Build, self).build_extensions()
         if not has_src:
             dir_util.remove_tree(include_path, dry_run=self.dry_run)
             dir_util.remove_tree(src_path, dry_run=self.dry_run)
@@ -176,7 +195,7 @@ setup(
     long_description=read("README.md"),
     long_description_content_type='text/markdown',
     url="https://github.com/KmolYuan/solvespace",
-    packages=find_packages(exclude=('tests',)),
+    packages=find_packages(exclude=('test',)),
     package_data={'': ["*.pyi", "*.pxd"], 'python_solvespace': ['py.typed']},
     ext_modules=[Extension(
         "python_solvespace.slvs",
