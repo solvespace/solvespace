6966098407fc75e4e8e21615590824001c1d8e62
Koen Schmeets <hello@koenschmeets.nl>
2020-10-19 11:24:37 +0200

    macOS CI fixes

--- .travis/build-macos.sh
+++ .travis/build-macos.sh
@@ -4,6 +4,6 @@ if echo $TRAVIS_TAG | grep ^v; then BUILD_TYPE=RelWithDebInfo; else BUILD_TYPE=D
 
 mkdir build
 cd build
-cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7 -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
-make
+cmake -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
+make -j2
 make test_solvespace
