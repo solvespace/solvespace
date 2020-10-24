8f509f14527f5f5776788f287f1d90ec446499b1
Koen Schmeets <hello@koenschmeets.nl>
2020-10-24 11:08:40 +0200

    Travis: CI improvements and fixing edge builds (#766)
    - Fix release notes by using edge deploy provider, see: https://github.com/travis-ci/dpl/pull/1069
    - Deploy only on master branch
    - Move to arm64-graviton2
    - Rename debian -> ubuntu
    - Remove appveyor.yml
    - Remove redundant deploy stage in build step names
    - Get rid of bash code in files using sh, and explicitly use bash in sign-macos.sh script
    - Add missing newline to build-windows.sh
    - Build x86 for Windows
    - Enable OpenMP in test builds
    - Disable sanitizers on macOS test build
    - Disallow failures on snap build

--- .travis/build-macos.sh
+++ .travis/build-macos.sh
@@ -11,18 +11,22 @@ export LDFLAGS="-L${LLVM_PREFIX}/lib -Wl,-rpath,${LLVM_PREFIX}/lib" \
 export CFLAGS="-I${LLVM_PREFIX}/include"
 export CPPFLAGS="-I${LLVM_PREFIX}/include"
 
-if [ "$1" == "release" ]; then
+if [ "$1" = "release" ]; then
     BUILD_TYPE=RelWithDebInfo
     cmake \
         -DCMAKE_OSX_DEPLOYMENT_TARGET="${OSX_TARGET}" \
-        -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" .. \
-        -DENABLE_OPENMP=ON \
-        -DENABLE_LTO=ON
+        -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+        -DENABLE_OPENMP="ON" \
+        -DENABLE_LTO="ON" \
+        ..
 else
     BUILD_TYPE=Debug
-        cmake \
+    cmake \
         -DCMAKE_OSX_DEPLOYMENT_TARGET="${OSX_TARGET}" \
-        -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" ..
+        -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
+        -DENABLE_OPENMP="ON" \
+        -DENABLE_SANITIZERS="ON" \
+        ..
 fi
 
 cmake --build . --config "${BUILD_TYPE}" -- -j$(nproc)
