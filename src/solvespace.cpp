58d06c61a62ad5905a4491ba061f4725ee36319c
ruevs <dpr@ruevs.com>
2020-10-26 00:12:29 +0200

    Update the year in the About dialog

--- src/solvespace.cpp
+++ src/solvespace.cpp
@@ -965,7 +965,7 @@ void SolveSpaceUI::MenuHelp(Command id) {
 "law. For details, visit http://gnu.org/licenses/\n"
 "\n"
 "© 2008-%d Jonathan Westhues and other authors.\n"),
-PACKAGE_VERSION, 2019);
+PACKAGE_VERSION, 2020);
             break;
 
         default: ssassert(false, "Unexpected menu ID");
