d2cf0a923a0c41d6581413c18330170ee25abe3a
Koen Schmeets <hello@koenschmeets.nl>
2020-12-08 22:28:00 +0100

    Benchmarks in CI

--- .github/scripts/build-ubuntu.sh
+++ .github/scripts/build-ubuntu.sh
@@ -1,11 +1,21 @@
 #!/bin/sh -xe
 
 mkdir build
-cd build
-cmake \
-  -DCMAKE_BUILD_TYPE="Debug" \
-  -DENABLE_OPENMP="ON" \
-  -DENABLE_SANITIZERS="ON" \
-  ..
+cd build || true
+
+if [ "$1" = "release" ]; then
+  cmake \
+    -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
+    -DENABLE_OPENMP="ON" \
+    -DENABLE_LTO="ON" \
+    ..
+else
+  cmake \
+    -DCMAKE_BUILD_TYPE="Debug" \
+    -DENABLE_OPENMP="ON" \
+    -DENABLE_SANITIZERS="ON" \
+    ..
+fi
+
 make -j$(nproc) VERBOSE=1
 make test_solvespace
