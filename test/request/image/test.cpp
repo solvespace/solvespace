0cd6fa9cc02871f78da98835e4285339d2c6a8d4
robnee <robert.nee@gmail.com>
2021-04-03 15:11:10 -0400

    Skip image rendering in CLI rather than hard abort

--- test/request/image/test.cpp
+++ test/request/image/test.cpp
@@ -2,13 +2,12 @@
 
 TEST_CASE(normal_roundtrip) {
     CHECK_LOAD("normal.slvs");
-    // Can't render images through cairo for now.
-    // CHECK_RENDER("normal.png");
+    CHECK_RENDER("normal.png");
     CHECK_SAVE("normal.slvs");
 }
 
 TEST_CASE(linked_roundtrip) {
     CHECK_LOAD("linked.slvs");
-    // CHECK_RENDER("linked.png");
+    CHECK_RENDER("linked.png");
     CHECK_SAVE("linked.slvs");
 }
