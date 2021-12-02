7774cc6e843882a0c9477b96583229cd7ba6076e
KmolYuan <pyslvs@gmail.com>
2021-12-02 12:11:42 +0800

    Bump version.

--- cython/python_solvespace/__init__.py
+++ cython/python_solvespace/__init__.py
@@ -8,7 +8,7 @@ __author__ = "Yuan Chang"
 __copyright__ = "Copyright (C) 2016-2019"
 __license__ = "GPLv3+"
 __email__ = "pyslvs@gmail.com"
-__version__ = "3.0.3.post1"
+__version__ = "3.0.4"
 
 from enum import IntEnum, auto
 from .slvs import (
