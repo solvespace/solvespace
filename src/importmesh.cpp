33271f127937b30d8f7bcbafc452f44b6776b796
phkahler <14852918+phkahler@users.noreply.github.com>
2022-01-23 10:42:40 -0500

    Check for text STL when linking

--- src/importmesh.cpp
+++ src/importmesh.cpp
@@ -130,6 +130,13 @@ bool LinkStl(const Platform::Path &filename, EntityList *el, SMesh *m, SShell *s
     char str[80] = {};
     f.read(str, 80);
     
+    if(str[0]=='s' && str[1]=='o' && str[2]=='l' && str[3]=='i' && str[4]=='d') {
+    // we could display a message that text stl files are not supported
+    // just returning false will trigger the warning that linked file is not present
+    // best solution is to add an importer for text STL.
+        return false;
+    }
+    
     uint32_t n;
     uint32_t color;
     
