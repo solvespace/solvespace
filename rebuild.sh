adaefb46aeccdba86ba4b3fbf851f1b9244863a7
Koen Schmeets <hello@koenschmeets.nl>
2023-02-21 09:16:55 +0100

    Increase available memory

--- rebuild.sh
+++ rebuild.sh
@@ -54,6 +54,35 @@ cat << EOF > index.html
     var y = slvs.getParamValue(p2, 1)
     console.log(39.54852, x)
     console.log(61.91009, y)
+
+
+    var r = 10
+    angle = 45
+    slvs.clearSketch()
+    var g = 1
+    var wp = slvs.add2DBase(g)
+    var p0 = slvs.addPoint2D(g, 0, 0, wp)
+    slvs.dragged(g, p0, wp)
+    var p1 = slvs.addPoint2D(g, 0, 10, wp)
+    slvs.distance(g, p0, p1, r, wp)
+    var line0 = slvs.addLine2D(g, p0, p1, wp)
+
+    var p2 = slvs.addPoint2D(g, 10, 10, wp)
+    var line1 = slvs.addLine2D(g, p1, p2, wp)
+    slvs.distance(g, p1, p2, r * (angle * (Math.PI / 180)), wp)
+    slvs.perpendicular(g, line0, line1, wp, false)
+
+    var p3 = slvs.addPoint2D(g, 10, 0, wp)
+    slvs.dragged(g, p3, wp)
+    var lineBase = slvs.addLine2D(g, p0, p3, wp)
+    slvs.angle(g, line0, lineBase, angle, wp, false)
+
+    var result = slvs.solveSketch(g, false)
+    console.log("result", result['result'])
+    var x = slvs.getParamValue(p2, 0)
+    var y = slvs.getParamValue(p2, 1)
+    console.log(12.62467, x)
+    console.log(1.51746, y)
   }
 </script>
 EOF
