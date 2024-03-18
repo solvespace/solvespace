c6b4c15d6724393eea9eb371a55a45ec9bbc3110
ruevs <dpr@ruevs.com>
2024-03-18 17:39:43 +0200

    Qt: Make the Qt version compile on Windows
    Tested working with AMD64 build with Visual Studio 19, Qt 5.12 and OpenGL 1
    
    Add:
    CMAKE_PREFIX_PATH = [path to your Qt]/Qt/Qt5.12.12/5.12.12/msvc2017_64/lib/cmake/Qt5
    to CMake to make it work.

--- src/platform/guiqt.cpp
+++ src/platform/guiqt.cpp
@@ -916,6 +916,11 @@ public:
     }
 };
 
+#if defined(WIN32)
+// This interferes with our identifier.
+#    undef CreateWindow
+#endif
+
 WindowRef CreateWindow(Window::Kind kind, WindowRef parentWindow) {
     return std::make_shared<WindowImplQt>(kind,
                 std::static_pointer_cast<WindowImplQt>(parentWindow));
