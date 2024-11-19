64b274bbad9393ddf55139a5944a06676e879fe4
phkahler <14852918+phkahler@users.noreply.github.com>
2024-11-19 16:23:43 -0500

    Add skewed subtype for extrusion groups. Update the textwindow radio buttons and equation generation.

--- src/textscreens.cpp
+++ src/textscreens.cpp
@@ -228,6 +228,7 @@ void TextWindow::ScreenChangeGroupOption(int link, uint32_t v) {
     switch(link) {
         case 's': g->subtype = Group::Subtype::ONE_SIDED; break;
         case 'S': g->subtype = Group::Subtype::TWO_SIDED; break;
+        case 'w': g->subtype = Group::Subtype::ONE_SKEWED; break;
 
         case 'k': g->skipFirst = true; break;
         case 'K': g->skipFirst = false; break;
@@ -377,14 +378,30 @@ void TextWindow::ShowGroupInfo() {
         Printf(true, " %Ft%s%E", s);
 
         bool one = (g->subtype == Group::Subtype::ONE_SIDED);
+        bool two = (g->subtype == Group::Subtype::TWO_SIDED);
+        bool skew = (g->subtype == Group::Subtype::ONE_SKEWED);
+        
+        if (g->type == Group::Type::EXTRUDE) {
+        Printf(false,
+            "%Ba   %f%Ls%Fd%s one-sided%E  "
+                  "%f%LS%Fd%s two-sided%E  "
+                  "%f%Lw%Fd%s skewed%E",
+            &TextWindow::ScreenChangeGroupOption,
+            one ? RADIO_TRUE : RADIO_FALSE,
+            &TextWindow::ScreenChangeGroupOption,
+            two ? RADIO_TRUE : RADIO_FALSE,
+            &TextWindow::ScreenChangeGroupOption,
+            skew ? RADIO_TRUE : RADIO_FALSE);
+        } else {
         Printf(false,
             "%Ba   %f%Ls%Fd%s one-sided%E  "
                   "%f%LS%Fd%s two-sided%E",
             &TextWindow::ScreenChangeGroupOption,
             one ? RADIO_TRUE : RADIO_FALSE,
             &TextWindow::ScreenChangeGroupOption,
-            !one ? RADIO_TRUE : RADIO_FALSE);
-
+            two ? RADIO_TRUE : RADIO_FALSE);        
+        }
+        
         if(g->type == Group::Type::ROTATE || g->type == Group::Type::TRANSLATE) {
             if(g->subtype == Group::Subtype::ONE_SIDED) {
                 bool skip = g->skipFirst;
