dd5a90f5187f927ea963b0cf5f43064dbc6cefcc
Jonathan Wyss <j.wyss@kolabnow.ch>
2025-01-12 11:36:33 +0100

    add handling for STL file instead of only stl file

--- src/file.cpp
+++ src/file.cpp
@@ -718,6 +718,8 @@ bool SolveSpaceUI::LoadEntitiesFromFile(const Platform::Path &filename, EntityLi
         return LinkIDF(filename, le, m, sh);
     } else if(strcmp(filename.Extension().c_str(), "stl")==0) {
         return LinkStl(filename, le, m, sh);    
+    } else if(strcmp(filename.Extension().c_str(), "STL")==0) {
+        return LinkStl(filename, le, m, sh);    
     } else {
         return LoadEntitiesFromSlvs(filename, le, m, sh);
     }
