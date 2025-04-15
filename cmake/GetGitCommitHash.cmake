2e979914351f37fde66ce19b0d2b405f9c2ff2f6
Isaac Garzon <ncebdacr@duck.com>
2025-04-15 21:58:40 +0300

    build: support getting git hash when imported as a submodule
    Useful for projects embedding solvespace as a submodule, so they wouldn't
    need to set the git hash manually.
    
    Taken from https://github.com/realthunder/solvespace

--- cmake/GetGitCommitHash.cmake
+++ cmake/GetGitCommitHash.cmake
@@ -3,6 +3,14 @@ function(get_git_commit_hash)
     get_filename_component(GIT_ROOT ${GIT_DESCRIBE_CMAKE_DIR} PATH)
     set(GIT_DIR "${GIT_ROOT}/.git")
 
+    if(EXISTS ${GIT_DIR} AND NOT IS_DIRECTORY ${GIT_DIR})
+        # In case we are included as a git submodule.
+        file(READ ${GIT_DIR} GIT_SUB_DIR)
+        string(REPLACE "gitdir: " "" GIT_SUB_DIR ${GIT_SUB_DIR})
+        string(STRIP ${GIT_SUB_DIR} GIT_SUB_DIR)
+        set(GIT_DIR "${GIT_ROOT}/${GIT_SUB_DIR}")
+    endif()
+
     # Add a CMake configure dependency to the currently checked out revision.
     set(GIT_DEPENDS ${GIT_DIR}/HEAD)
     file(READ ${GIT_DIR}/HEAD HEAD_REF)
