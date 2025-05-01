2f89373ba608097a5f25fbfccb9e69922cfccc8b
Isaac Garzon <ncebdacr@duck.com>
2025-05-01 15:28:50 +0300

    general: use the ParamList and EntityList type aliases
    They are easier to read, and this makes the code more consistent.

--- src/request.cpp
+++ src/request.cpp
@@ -78,8 +78,7 @@ Request::Type EntReqTable::GetRequestForEntity(Entity::Type ent) {
     return req;
 }
 
-void Request::Generate(IdList<Entity,hEntity> *entity,
-                       IdList<Param,hParam> *param)
+void Request::Generate(EntityList *entity, ParamList *param)
 {
     int points = 0;
     Entity::Type et = (Entity::Type)0;
@@ -239,7 +238,7 @@ int Request::IndexOfPoint(hEntity he) const {
     return -1;
 }
 
-hParam Request::AddParam(IdList<Param,hParam> *param, hParam hp) {
+hParam Request::AddParam(ParamList *param, hParam hp) {
     Param pa = {};
     pa.h = hp;
     param->Add(&pa);
