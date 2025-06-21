e19bdf81d085d93179e0818fada13b8d2359b9bc
ruevs <dpr@ruevs.com>
2025-06-21 14:08:21 +0300

    CI, Win32: Add x64 (64bit) binaries for Windows
    By popular demand the CI now builds 64bit binaries for Windows.
    This allows SolveSpace to open large models (by accessing >4GB RAM).
    
    At the same time rename the default versions lie this:
    
    bit/thread  | x86 (32bit) | x64 (64bit)
    ------ | ----- | ------
    Single thread | solvespace_single_core.exe | solvespace_single_core_x64.exe
    Multi thread | solvespace.exe | solvespace_x64.exe
    
    Fixes #1261

--- .github/scripts/build-windows.sh
+++ .github/scripts/build-windows.sh
@@ -9,13 +9,20 @@ if [ "$1" = "release" ]; then
     else
         ENABLE_OPENMP="OFF"
     fi
+
+    if [ "$3" = "x64" ]; then
+        PLATFORM="$3"
+    else
+        PLATFORM="Win32"
+    fi
+
     BUILD_TYPE=RelWithDebInfo
     cmake \
         -G "Visual Studio 17 2022" \
         -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" \
         -DENABLE_OPENMP="${ENABLE_OPENMP}" \
         -DENABLE_LTO=ON \
-        -DCMAKE_GENERATOR_PLATFORM="Win32" \
+        -DCMAKE_GENERATOR_PLATFORM="${PLATFORM}" \
         ..
 else
     BUILD_TYPE=Debug
@@ -31,6 +38,14 @@ cmake --build . --config "${BUILD_TYPE}" -- -maxcpucount
 
 bin/$BUILD_TYPE/solvespace-testsuite.exe
 
-if [ "$2" = "openmp" ]; then
-    mv bin/$BUILD_TYPE/solvespace.exe bin/$BUILD_TYPE/solvespace-openmp.exe
+if [ "$3" = "x64" ]; then
+	if [ "$2" != "openmp" ]; then
+		mv bin/$BUILD_TYPE/solvespace.exe bin/$BUILD_TYPE/solvespace_single_core_x64.exe
+	else
+		mv bin/$BUILD_TYPE/solvespace.exe bin/$BUILD_TYPE/solvespace_x64.exe
+	fi
+else
+	if [ "$2" != "openmp" ]; then
+		mv bin/$BUILD_TYPE/solvespace.exe bin/$BUILD_TYPE/solvespace_single_core.exe
+	fi
 fi
